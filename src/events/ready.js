79182333abd4d540d290b29531f564276bf7ab27
TK-825 <huybyvycyct@gmail.com>
2026-09-24 15:56:18 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -17,9 +17,6 @@ export default {
       startupLog(`Serving ${client.guilds.cache.size} guild(s)`);
       startupLog(`Loaded ${client.commands.size} commands`);
 
-      if (client.config?.features?.music) {
-        initRiffyAfterReady(client);
-      }
 
       const reconciliationSummary = await reconcileReactionRoleMessages(client);
       startupLog(
