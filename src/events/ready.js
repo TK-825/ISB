bc38ff1ffbdbf68f02b8d859f6483a8b65161d22
TK-825 <huybyvycyct@gmail.com>
2026-09-24 15:49:57 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,7 @@
 import { Events } from "discord.js";
 import { logger, startupLog } from "../utils/logger.js";
 import config from "../config/application.js";
+import { initRiffyAfterReady } from '../utils/riffy.js';
 import { reconcileReactionRoleMessages } from "../services/reactionRoleService.js";
 import { reconcileTicketPanels, reconcileVerificationPanels, reconcileReactionRolePanelHealth } from "../services/panelHealthService.js";
 import { reconcileLevelRoles } from "../services/levelRoleSyncService.js";
